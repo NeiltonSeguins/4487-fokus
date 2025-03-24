0c38da89e21de93a0fb8a614e90b792a097842f6
NeiltonSeguins <seguins.neilton@gmail.com>
2025-03-24 11:04:26 -0300

    fix: fazendo ajustes nos componentes.

--- src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
+++ src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css";
-export default function BotaoModo({ children, modoBotao }) {
+export default function BotaoModo({ children }) {
   return (
     <button
       className={`
--- src/components/Cronometro/BotoesModos/index.jsx
+++ src/components/Cronometro/BotoesModos/index.jsx
@@ -3,18 +3,34 @@ import styles from "./styles.module.css";
 import BotaoModo from "./BotaoModo";
 
 export default function BotoesModos() {
-  const modo = {
-    id: "foco",
-    nome: "Foco",
-    frase: ["Otimize sua produtividade,", "mergulhe no que importa."],
-    tempoInicialEmSegundos: 30,
-  };
+  const modo = [
+    {
+      id: "foco",
+      nome: "Foco",
+      frase: ["Otimize sua produtividade,", "mergulhe no que importa."],
+      tempoInicialEmSegundos: 25,
+    },
+    {
+      id: "pausa_curta",
+      nome: "Pausa curta",
+      frase: ["Que tal dar uma respirada?", "Faça uma pausa curta"],
+      tempoInicialEmSegundos: 5,
+    },
+    {
+      id: "pausa_longa",
+      nome: "Pausa longa",
+      frase: ["Hora de voltar a superfície", "Faça uma pausa longa."],
+      tempoInicialEmSegundos: 15,
+    },
+  ];
 
   return (
     <ul className={styles["cronometer-modes"]}>
-      <li key={modo.id}>
-        <BotaoModo modoBotao={modo}>{modo.nome}</BotaoModo>
-      </li>
+      {modo.map((m) => (
+        <li key={m.id}>
+          <BotaoModo>{m.nome}</BotaoModo>
+        </li>
+      ))}
     </ul>
   );
 }
